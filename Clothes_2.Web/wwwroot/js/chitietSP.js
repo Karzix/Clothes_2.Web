3b6e4e7cdaf7ef4a351bf6265b06631487ababa7
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-06-09 21:33:54 +0700

    upload

--- Clothes_2.Web/wwwroot/js/chitietSP.js
+++ Clothes_2.Web/wwwroot/js/chitietSP.js
@@ -1,6 +1,16 @@
 
-function backHistory(){
-    history.back();
+
+function upslhang() {
+    var sl = Number(document.getElementById('SL').value);
+    sl++;
+    document.getElementById('SL').value = sl;
+}
+function downslhang() {
+    var sl = Number(document.getElementById('SL').value);
+    sl--;
+    if (sl >= 1) {
+        document.getElementById('SL').value = sl
+    }
 }
 // var tang= document.getElementById('tang');
 // tang.addEventListener('click',funtiontang());
